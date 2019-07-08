90e07812ccbfa59752bdd0a96b22b2616b3564b7
Sidz <srikanth.siddhu@gmail.com>
2019-07-08 09:28:58 +0530

    Initial Commit

---
+++ Source/Assets/_GeneratedFiles/SampleGenerated.cs
@@ -0,0 +1,12 @@
+// ----- ZDIS_Unity_Tool:AUTO GENERATED CODE ----- //
+// ----- Created through ClassGenerator ----- //
+// ----- Don't edit this class ----- //
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+/*---------------------------------------------------*/
+public  class SampleGenerated
+{
+//Hi you did it!
+}
